5e97c60cc4630d7c7eebc54bb75fa95be3b2c1c0
Ricardo T <ricardo85x@gmail.com>
2024-03-10 19:45:33 -0300

    feat: fix process bg color

--- src/pages/monitor/components/monitor-service.tsx
+++ src/pages/monitor/components/monitor-service.tsx
@@ -11,8 +11,8 @@ type MonitorServiceProps = {
 export const MonitorService: React.FC<MonitorServiceProps> = ({ newProcesses, removedProcesses, showNewProcessAnimation, showRemovedProcessAnimation }) => {
     return (
         <div className="flex flex-col sm:flex-row gap-4 p-6 text-white max-w-2xl w-full mx-auto">
-            <ProcessList title="New Processes" processes={newProcesses} animation={showNewProcessAnimation} color="green" />
-            <ProcessList title="Ended Processes" processes={removedProcesses} animation={showRemovedProcessAnimation} color="red" />
+            <ProcessList title="New Processes" processes={newProcesses} animation={showNewProcessAnimation} type="new" />
+            <ProcessList title="Ended Processes" processes={removedProcesses} animation={showRemovedProcessAnimation} type="ended" />
         </div>
     );
 };
--- src/pages/monitor/components/process-list.tsx
+++ src/pages/monitor/components/process-list.tsx
@@ -4,39 +4,19 @@ type ProcessListProps = {
     title: string;
     processes: Process[];
     animation: boolean;
-    color: string;
+    type: "new" | "ended";
 };
 
-export const ProcessListOld: React.FC<ProcessListProps> = ({ title, processes, animation, color }) => {
+export const ProcessList: React.FC<ProcessListProps> = ({ title, processes, animation, type }) => {
     return (
         <div className="flex flex-col gap-4">
             <div className="text-lg font-bold">{title}</div>
             {processes.map((process) => (
-                <div key={process.id} className={`flex min-w-72 bg-${color}-700 rounded-lg shadow-md ${animation && process.isNew ? "animate-pulse" : ""}`}>
-                    <div className={`w-fit p-4 bg-${color}-800 rounded-l-lg flex justify-center items-center`}>
+                <div key={process.id} className={`rounded-lg shadow-md flex min-w-72 ${type === 'new' ? 'bg-green-700' : 'bg-red-700'} ${animation && process.isNew ? 'animate-pulse' : ''}  `}>
+                    <div className={`w-fit p-4 ${type === 'new' ? 'bg-green-800' : 'bg-red-800'} rounded-l-lg flex justify-center items-center`}>
                         <div className="text-sm">{process.id}</div>
                     </div>
-                    <div className="flex-grow p-4 bg-${color}-700 rounded-r-lg">
-                        <div className="font-bold">{process.name}</div>
-                        <div className="text-sm">{new Date(process.date).toLocaleString()}</div>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
-};
-
-export const ProcessList: React.FC<ProcessListProps> = ({ title, processes, animation, color }) => {
-    console.log('animation', animation)
-    return (
-        <div className="flex flex-col gap-4">
-            <div className="text-lg font-bold">{title}</div>
-            {processes.map((process, index) => (
-                <div key={process.id} style={{ transitionDelay: `${index * 0.1}s` }} className={`transition-all duration-300 flex min-w-72 bg-${color}-700 rounded-lg shadow-md `}>
-                    <div className={`w-fit p-4 bg-${color}-800 rounded-l-lg flex justify-center items-center`}>
-                        <div className="text-sm">{process.id}</div>
-                    </div>
-                    <div className="flex-grow p-4 bg-${color}-700 rounded-r-lg">
+                    <div className={`flex-grow p-4 ${type === 'new' ? 'bg-green-700' : 'bg-red-700'}  rounded-r-lg`}>
                         <div className="font-bold">{process.name}</div>
                         <div className="text-sm">{new Date(process.date).toLocaleString()}</div>
                     </div>
